46da4d852bb0c753142bfd8ae6bf1568bdeb0361
Abhignanendra Penugonda <abhignanendra.p@gmail.com>
2025-08-13 02:48:42 +0530

    some more changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/my_learning_notes/' // Set the base path for GitHub Pages
+});
