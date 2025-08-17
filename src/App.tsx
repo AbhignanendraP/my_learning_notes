1a4792244e3e3f08c4774391c3b4188b0a940eb0
Abhignanendra Penugonda <abhignanendra.p@gmail.com>
2025-08-18 03:14:45 +0530

    drug intro v4

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Container, Row, Col } from "react-bootstrap";
 import "./App.css";
 import capsule_1 from "./assets/capsule.png";
+import round_tablet_1 from "./assets/round_table_1.png";
 import { API_value } from "./App_data_api";
+import ReactMarkdown from "react-markdown";
+import Skeleton from "@mui/material/Skeleton";
+import Stack from "@mui/material/Stack";
+import drug_intro_image from "./assets/drug_intro_image.png";
 
 function App() {
   const handleSelectItem = (item: string) => {
@@ -12,6 +17,7 @@ function App() {
   };
   const introduction = API_value("introduction");
   const quickNote = API_value("quick_note");
+  const drug_introduction = API_value("drug_introduction");
   return (
     <div>
       {/* HEADER */}
@@ -23,6 +29,21 @@ function App() {
             className="img-fluid custom-image"
           />
         </div>
+        <div className="capsule_2">
+          <img
+            src={capsule_1}
+            alt="capsule 2"
+            className="img-fluid custom-image"
+          />
+        </div>
+        <div className="round_tablet_1">
+          <img
+            src={round_tablet_1}
+            alt="capsule 1"
+            className="img-fluid custom-image"
+          />
+        </div>
+
         <div className="container-fluid header_content">
           <div className="row header-bar">
             <h1 className="header-title">MY LEARNING NOTES</h1>
@@ -31,21 +52,56 @@ function App() {
 
           {/* CONTENT (floating card) */}
           <div className="row content">
-            <div className="intro-card-fluid shadow">
-              <h4 className="text-center fw-bold">Hi Everyone!</h4>
-              <p className="text-center">{introduction ?? "Loading..."}</p>
+            <div className="intro-card-fluid">
+              {drug_introduction ? (
+                <>
+                  <h4 className="text-center fw-bold">Hi Everyone!</h4>
+                  <p className="text-center">{introduction}</p>
 
-              <div className="text-center">
-                <span className="quick-note">
-                  <b>A QUICK NOTE:</b> {quickNote ?? "Loading..."}
-                </span>
-              </div>
+                  <div className="quick-note">
+                    <b className="me-2">A QUICK NOTE:</b>
+                    <p className="mb-0">{quickNote}</p>
+                  </div>
 
-              <p className="text-center">
-                I'll begin by sharing what I'm learning about the pharma domain,
-                and I hope this content helps others who are also starting their
-                journey in this field.
-              </p>
+                  <p className="text-center">
+                    I'll begin by sharing what I'm learning about the pharma
+                    domain, and I hope this content helps others who are also
+                    starting their journey in this field.
+                  </p>
+                  <div
+                    className="text drug_introduction"
+                    style={{ whiteSpace: "pre-wrap" }}
+                  >
+                    <img
+                      src={drug_intro_image}
+                      alt="API and Excipients division"
+                      className="drug-image-float"
+                    />
+                    <ReactMarkdown>{drug_introduction ?? ""}</ReactMarkdown>
+                  </div>
+                </>
+              ) : (
+                <Stack spacing={1}>
+                  <Skeleton
+                    variant="rounded"
+                    animation="wave"
+                    width={"100%"}
+                    height={"3vw"}
+                  />
+                  <Skeleton
+                    variant="rounded"
+                    animation="wave"
+                    width={"100%"}
+                    height={"2vw"}
+                  />
+                  <Skeleton
+                    variant="rounded"
+                    animation="wave"
+                    width={"100%"}
+                    height={"10vw"}
+                  />
+                </Stack>
+              )}
             </div>
           </div>
         </div>
